7301d5854b7461cb7664463bcabb10f09ea854d4
Bin Cai <imcaibin@gmail.com>
2012-05-24 23:22:56 +0100

    block IE8 and blew

---
+++ source/javascripts/ie.js
@@ -0,0 +1,4 @@
+$(function() {
+	$('body').html('<p class="ie">Sorry, your web browser is out of fashion and not supported by this website. Please update to Google Chrome, Firefox, Safari or IE9. <br />还在使用过时的浏览器？不好意思，本站不支持您的浏览器，请升级到Google Chrome、Firefox、Safari或者IE9。</p>');
+});
+
