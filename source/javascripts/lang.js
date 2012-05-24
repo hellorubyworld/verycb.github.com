b0fdee8ed86e2a3af539da330fd6d758b7463c9a
Bin Cai <imcaibin@gmail.com>
2012-05-24 16:37:19 +0100

    add avatar and email

--- source/javascripts/lang.js
+++ source/javascripts/lang.js
@@ -1,6 +1,6 @@
 $(function() {
-	$('header a').click(function () {
-		$('header a').removeClass('current');
+	$('header ul li a').click(function () {
+		$('header ul li a').removeClass('current');
 		$(this).addClass('current');
 		$('section').addClass('hidden');
 		$('#intro-' + this.id).removeClass('hidden');
