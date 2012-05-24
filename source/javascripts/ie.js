bb8e74d1751da3a5149b5df9aacfeda5773fb040
Bin Cai <imcaibin@gmail.com>
2012-05-24 23:59:06 +0100

    change ie style

--- source/javascripts/ie.js
+++ source/javascripts/ie.js
@@ -1,4 +1,4 @@
 $(function() {
-	$('body').html('<p class="ie">Sorry, your web browser is out of fashion and not supported by this website. Please update to Google Chrome, Firefox, Safari or IE9. <br /><br />还在使用过时的浏览器？不好意思，它没有得到本站的支持，请升级到Google Chrome、Firefox、Safari或者IE9。</p>');
+	$('body').html('<p id="ie">Sorry, your web browser is out of fashion and not supported by this website. Please update to Google Chrome, Firefox, Safari or IE9. <br /><br />还在使用过时的浏览器？不好意思，它没有得到本站的支持，请升级到Google Chrome、Firefox、Safari或者IE9。</p>');
 });
 
